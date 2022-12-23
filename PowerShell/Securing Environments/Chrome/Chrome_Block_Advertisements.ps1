29a2c0db59a887e3a22356fe150d489dd5fe49fc
Chris M <36876011+0Chris-M@users.noreply.github.com>
2022-12-24 10:12:58 +1100

    Update and rename PowerShell/Securing Environments/Chrome_Block_Advertisements.ps1 to PowerShell/Securing Environments/Chrome/Chrome_Block_Advertisements.ps1

---
+++ PowerShell/Securing Environments/Chrome/Chrome_Block_Advertisements.ps1
@@ -0,0 +1,6 @@
+$chrmext = Test-Path -Path HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist
+If ($chrmext -eq $False)
+{
+	New-Item -Path HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist -Force
+}
+Set-ItemProperty -Path HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist -Name 1 -Value "cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx" -Type String -Force
--- PowerShell/Securing Environments/Chrome_Block_Advertisements.ps1
+++
@@ -1,6 +0,0 @@
-$cmnsec = Test-Path -Path HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist
-If ($cmnsec -eq $False)
-{
-	New-Item -Path HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist -Force
-}
-Set-ItemProperty -Path HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist -Name 1 -Value "cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx" -Type String -Force
