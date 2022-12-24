2a8c9cf7120f74aceff70f99b1c51f05fa5a82ed
Chris M <36876011+0Chris-M@users.noreply.github.com>
2022-12-24 12:20:31 +1100

    Update Local_Admin_Check.ps1

--- PowerShell/Auditing/Local_Admin_Check.ps1
+++ PowerShell/Auditing/Local_Admin_Check.ps1
@@ -1,6 +1,6 @@
 # Pulls list of any Local Admin account present on the machine
 
-# If Windows OS isn't at least Win10 v1607 or Server 2016 or PowerShell < v5.1, then exit 
+# If PowerShell < v5.1, then exit 
 if ($PSVersionTable.PSVersion -lt [version]'5.1') { Exit 0 }
 $adminNames = Get-LocalGroupMember -Group Administrators
 If ($adminNames) { Return $adminNames } else { Exit 0 }
